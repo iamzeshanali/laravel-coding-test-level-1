d0c114bb25f3f7937fb33abb3d73b0e3d876f3e7
Zeeshan Ali <zeeshanalibenifshi@gmai.com>
2022-10-15 02:49:22 +0500

    1-crud-api task completed

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EventController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['prefix' => 'v1'], function(){
+    Route::get('events/active-events', [EventController::class, 'activeEvents']);
+    Route::apiResource('events', EventController::class);
+    Route::patch('events/{event}', [EventController::class, 'partiallyUpdate']);
+
 });
